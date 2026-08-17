93b1146aeba6831d8a9df198658fac4029202516
arinrathore <168368497+devarintakmashido@users.noreply.github.com>
2026-08-17 16:56:09 +0530

    Delete 03LibraryAPI directory

--- 03LibraryAPI/src/main/java/com/example/_LibraryAPI/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example._LibraryAPI.repository;
-
-import com.example._LibraryAPI.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
